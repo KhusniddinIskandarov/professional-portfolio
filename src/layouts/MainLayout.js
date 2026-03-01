0898f88bbca326c743c644eb8ac5172d1b4d8034
KhusniddinIskandarov <kh.sh.iskandarov@gmail.com>
2026-03-01 19:42:52 +0500

    feat: add contact section and footer with responsive design

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,6 +2,8 @@ import { Navbar } from '../components/sections/Navbar'
 import { HeroSection } from '../components/sections/HeroSection'
 import { AboutSection } from '../components/sections/AboutSection'
 import { ProjectsSection } from '../components/sections/ProjectsSection'
+import { ContactSection } from '../components/sections/ContactSection'
+import { Footer } from '../components/sections/Footer'
 
 export const MainLayout = () => {
   return `
@@ -12,7 +14,10 @@ export const MainLayout = () => {
     ${HeroSection()}
     ${AboutSection()}
     ${ProjectsSection()}
+    ${ContactSection()}
     </main>
-    <footer id="footer"></footer>
+    <footer>
+    ${Footer()}
+    </footer>
   `
 }
